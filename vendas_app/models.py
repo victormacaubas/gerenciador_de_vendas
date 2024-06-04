7e5c246689333863234a1f46ca222e6910214a58
Victor Macaubas <victor@macaubas.com>
2024-06-04 17:48:01 -0300

    criação dos modelos

--- vendas_app/models.py
+++ vendas_app/models.py
@@ -1,29 +1,81 @@
 from django.db import models
+from django.db.models import Q
 
-class cliente(models.Model):
-    SEXO_CHOICES = [
-        ('M', 'Masculino'),
-        ('F', 'Feminino'),
-        ('O', 'Outro'),
+# Create your models here.
+GENDER_CHOICES = [
+        ('m', 'Masculino', 'M'),
+        ('f', 'Feminino', 'F'),
+        ('o', 'Outro', 'O'),
     ]
 
+ROLE_CHOICES = [
+        ('gerente', 'Gerente'), 
+        ('vendedor', 'Vendedor'), 
+        ('ceo', 'CEO')
+    ]
+
+class Cliente(models.Model):
+
     nome = models.CharField(max_length=100)
-    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
+    sexo = models.CharField(max_length=1)
     idade = models.IntegerField()
     nascimento = models.DateField()
 
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name='check_gender',
+                check=Q(sexo__in=GENDER_CHOICES),
+                violation_error_message='Sexo inválido, escolha entre masculino, feminino ou outro.'
+            )
+        ]
+
 class ClienteEspecial(models.Model):
-    SEXO_CHOICES = [
-        ('M', 'Masculino'),
-        ('F', 'Feminino'),
-        ('O', 'Outro'),
-    ]
 
     nome = models.CharField(max_length=100)
-    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
     idade = models.IntegerField()
-    cliente = models.ForeignKey(cliente, on_delete=models.CASCADE)
+    sexo = models.CharField(max_length=1)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     cashback = models.DecimalField(max_digits=5, decimal_places=2)
 
 
-# Create your models here.
+class Funcionario(models.Model):
+
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    sexo = models.CharField(max_length=1)
+    cargo = models.CharField(max_length=100)
+    salario = models.DecimalField(max_digits=5, decimal_places=2)
+    nascimento = models.DateField()
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name='check_role',
+                check=Q(cargo__in=ROLE_CHOICES),
+                violation_error_message='Cargo inválido, escolha entre gerente, vendedor ou CEO.'
+            )
+        ]
+
+class Produto(models.Model):
+
+    nome = models.CharField(max_length=100)
+    quantidade = models.IntegerField()
+    descricao = models.TextField()
+    valor = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name='check_value',
+                check=Q(valor__gte=0),
+                violation_error_message='Valor do produto deve ser maior ou igual a zero.'
+            )
+        ]
+
+class Venda(models.Model):
+
+    id_vendedor = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
+    id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    data = models.DateField()
+
