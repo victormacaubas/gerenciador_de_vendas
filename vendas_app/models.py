f485afb58b77993734b7c98e5dd721c7ce2f3d75
Victor Macaubas <victor@macaubas.com>
2024-06-04 19:23:20 -0300

    update models

--- vendas_app/models.py
+++ vendas_app/models.py
@@ -3,15 +3,15 @@ from django.db.models import Q
 
 # Create your models here.
 GENDER_CHOICES = [
-        ('m', 'Masculino', 'M'),
-        ('f', 'Feminino', 'F'),
-        ('o', 'Outro', 'O'),
+        ("m", "M"),
+        ("f", "F"),
+        ("o", "O"),
     ]
 
 ROLE_CHOICES = [
-        ('gerente', 'Gerente'), 
-        ('vendedor', 'Vendedor'), 
-        ('ceo', 'CEO')
+        ("gerente", "Gerente"), 
+        ("vendedor", "Vendedor"), 
+        ("ceo", "CEO")
     ]
 
 class Cliente(models.Model):
@@ -25,7 +25,7 @@ class Cliente(models.Model):
         constraints = [
             models.CheckConstraint(
                 name='check_gender',
-                check=Q(sexo__in=GENDER_CHOICES),
+                check=Q(sexo = 'm') | Q(sexo = 'f') | Q(sexo = 'o'),
                 violation_error_message='Sexo inválido, escolha entre masculino, feminino ou outro.'
             )
         ]
@@ -42,7 +42,7 @@ class ClienteEspecial(models.Model):
         constraints = [
             models.CheckConstraint(
                 name='check_gender_cliente_especial',
-                check=Q(sexo__in=GENDER_CHOICES),
+                check=Q(sexo = 'm') | Q(sexo = 'f') | Q(sexo = 'o'),
                 violation_error_message='Sexo inválido, escolha entre masculino, feminino ou outro.'
             )
         ]
@@ -62,12 +62,12 @@ class Funcionario(models.Model):
         constraints = [
             models.CheckConstraint(
                 name='check_role',
-                check=Q(cargo__in=ROLE_CHOICES),
+                check=Q(cargo = 'gerente') | Q(cargo = 'vendedor') | Q(cargo = 'ceo'),
                 violation_error_message='Cargo inválido, escolha entre gerente, vendedor ou CEO.'
             ),
             models.CheckConstraint(
                 name='check_gender_funcionario',
-                check=Q(sexo__in=GENDER_CHOICES),
+                check=Q(sexo = 'm') | Q(sexo = 'f') | Q(sexo = 'o'),
                 violation_error_message='Sexo inválido, escolha entre masculino, feminino ou outro.'
             )
         ]
