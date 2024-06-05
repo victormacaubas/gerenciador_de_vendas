acddf502b6212f0011355a03270e9868d7614c45
Victor Macaubas <victor@macaubas.com>
2024-06-05 17:13:20 -0300

    add delete permissions

--- vendas_app/admin.py
+++ vendas_app/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from .models import Cliente, ClienteEspecial, Funcionario, Produto, Venda
+from django.http import HttpRequest
+from .models import Cliente,Produto, Venda
 
-# Register your models here.
-admin.site.register(Cliente)
-admin.site.register(Funcionario)
-admin.site.register(Produto)
-admin.site.register(Venda)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'quantidade', 'valor', 'descricao')
+    search_fields = ('nome',)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'idade', 'sexo', 'nascimento')
+    search_fields = ('nome',)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+class VendaAdmin(admin.ModelAdmin):
+    list_display = ('id_produto', 'id_vendedor', 'id_cliente', 'quantidade')
+    search_fields = ('vendedor__nome', 'cliente__nome')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Venda, VendaAdmin)
 
