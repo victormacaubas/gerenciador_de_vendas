d25c63a094a1a9ec40bbe6dde104e4f90670ada7
Victor Macaubas <victor@macaubas.com>
2024-06-05 17:58:04 -0300

    created users and groups

--- vendas_app/admin.py
+++ vendas_app/admin.py
@@ -7,21 +7,43 @@ class ProdutoAdmin(admin.ModelAdmin):
     search_fields = ('nome',)
 
     def has_delete_permission(self, request, obj=None):
-        return False
+        if request.user.groups.filter(name='funcionario').exists():
+            return False
+        return super().has_delete_permission(request, obj)
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.groups.filter(name='funcionario').exists():
+            return False
+        return super().has_change_permission(request, obj)
     
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nome', 'idade', 'sexo', 'nascimento')
     search_fields = ('nome',)
 
     def has_delete_permission(self, request, obj=None):
-        return False
+        if request.user.groups.filter(name='funcionario').exists():
+            return False
+        return super().has_delete_permission(request, obj)
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.groups.filter(name='funcionario').exists():
+            return False
+        return super().has_change_permission(request, obj)
+
 
 class VendaAdmin(admin.ModelAdmin):
     list_display = ('id_produto', 'id_vendedor', 'id_cliente', 'quantidade')
     search_fields = ('vendedor__nome', 'cliente__nome')
 
     def has_delete_permission(self, request, obj=None):
-        return False
+        if request.user.groups.filter(name='funcionario').exists():
+            return False
+        return super().has_delete_permission(request, obj)
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.groups.filter(name='funcionario').exists():
+            return False
+        return super().has_change_permission(request, obj)
 
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Produto, ProdutoAdmin)
