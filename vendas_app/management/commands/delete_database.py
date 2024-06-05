e9244b0eccda2390ab7accf6076a0be76484edbb
Victor Macaubas <victor@macaubas.com>
2024-06-05 15:08:01 -0300

    update delete database command

--- vendas_app/management/commands/delete_database.py
+++ vendas_app/management/commands/delete_database.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
             cursor.execute("SET FOREIGN_KEY_CHECKS = 0;")
             cursor.execute("SHOW TABLES;")
             for table in cursor.fetchall():
-                cursor.execute(f"DROP TABLE IF EXISTS '{table[0]}';")
+                cursor.execute(f"DROP TABLE IF EXISTS {table[0]};")
             cursor.execute("SET FOREIGN_KEY_CHECKS = 1;")
 
         self.stdout.write(self.style.SUCCESS('Banco de dados deletado com sucesso!'))
