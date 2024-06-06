f716f59715343014bf5f44814c15ec82db695817
Victor Macaubas <victor@macaubas.com>
2024-06-06 20:46:09 -0300

    add script to create default users

---
+++ vendas_app/management/commands/create_users_and_groups.py
@@ -0,0 +1,71 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission, User
+from django.contrib.contenttypes.models import ContentType
+from vendas_app.models import Cliente, ClienteEspecial, Funcionario, Produto, Venda
+from decouple import config
+
+class Command(BaseCommand):
+    help = 'Create default users and groups'
+
+    def handle(self, *args, **kwargs):
+        gerente_password = config('GERENTE_PASSWORD')
+        funcionario_password = config('FUNCIONARIO_PASSWORD')
+
+        gerente_group, created = Group.objects.get_or_create(name='gerente')
+
+        permissions = Permission.objects.filter(
+            content_type__in=[
+                ContentType.objects.get_for_model(Cliente),
+                ContentType.objects.get_for_model(ClienteEspecial),
+                ContentType.objects.get_for_model(Funcionario),
+                ContentType.objects.get_for_model(Produto),
+                ContentType.objects.get_for_model(Venda),
+            ]
+        )
+        gerente_group.permissions.set(permissions)
+
+        funcionario_group, created = Group.objects.get_or_create(name='funcionario')
+
+        funcionario_group.permissions.set(
+            Permission.objects.filter(
+                codename__startswith='add'
+            ) | Permission.objects.filter(
+                codename__startswith='view'
+            )
+        )
+
+        funcionario_group.permissions.remove(
+            *Permission.objects.filter(codename__startswith='delete')
+        )
+        funcionario_group.permissions.remove(
+            *Permission.objects.filter(codename__startswith='change')
+        )
+
+        print('Groups and permissions set up successfully.')
+
+        # Create 'gerente' user
+        gerente_user = User.objects.create_user(
+            username='gerente',
+            password=gerente_password,
+            email='gerente@paladins.com'
+        )
+        gerente_user.groups.add(gerente_group)
+
+        # Create 'funcionario' user
+        funcionario_user = User.objects.create_user(
+            username='funcionario',
+            password=funcionario_password,
+            email='funcionario@paladins.com'
+        )
+        funcionario_user.groups.add(funcionario_group)
+
+        gerente_user = User.objects.get(username='gerente')
+        gerente_user.is_staff = True
+        gerente_user.save()
+
+        # Ensure funcionario user is active
+        funcionario_user = User.objects.get(username='funcionario')
+        funcionario_user.is_staff = True
+        funcionario_user.save()
+
+        self.stdout.write(self.style.SUCCESS('Successfully created users and groups'))
