39218d25d4b7c0800f19ff868e1d105d8e8b1e7f
Victor Macaubas <victor@macaubas.com>
2024-06-05 22:50:02 -0300

    protect local test variables

--- gerenciador_de_vendas/settings.py
+++ gerenciador_de_vendas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'gerenciador_de_vendas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'paladins_db',
-        'USER' : 'djangoApp',
-        'PASSWORD' : 'Kk#47JlP56m@c@',
-        'HOST' : 'localhost',
-        'PORT' : '3306',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
