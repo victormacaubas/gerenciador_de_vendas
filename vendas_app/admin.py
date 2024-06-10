1d43d234c1fb8e4be5d13a88c053a92c22c69dd7
igor-c-assuncao <igor.00000829605@unicap.br>
2024-06-10 16:24:16 -0300

    atualizando adm . py

--- vendas_app/admin.py
+++ vendas_app/admin.py
@@ -1,52 +1,68 @@
 from django.contrib import admin
+from django.urls import path
 from django.http import HttpRequest
 from .models import Cliente,Produto, Venda
-
+from .views import total_revenue_by_vendor_view, monthly_sales_by_product_view, top_clients_view, estatisticas_view
+ 
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'quantidade', 'valor', 'descricao')
     search_fields = ('nome',)
-
+ 
     def has_delete_permission(self, request, obj=None):
         if request.user.groups.filter(name='funcionario').exists():
             return False
         return super().has_delete_permission(request, obj)
-
+ 
     def has_change_permission(self, request, obj=None):
         if request.user.groups.filter(name='funcionario').exists():
             return False
         return super().has_change_permission(request, obj)
-    
+   
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nome', 'idade', 'sexo', 'nascimento')
     search_fields = ('nome',)
-
+ 
     def has_delete_permission(self, request, obj=None):
         if request.user.groups.filter(name='funcionario').exists():
             return False
         return super().has_delete_permission(request, obj)
-
+ 
     def has_change_permission(self, request, obj=None):
         if request.user.groups.filter(name='funcionario').exists():
             return False
         return super().has_change_permission(request, obj)
-
-
+ 
+ 
 class VendaAdmin(admin.ModelAdmin):
     list_display = ('produto', 'vendedor', 'cliente', 'quantidade','valor','data')
     search_fields = ('vendedor__nome', 'cliente__nome')
     exclude = ('valor',)
-
+ 
     def has_delete_permission(self, request, obj=None):
         if request.user.groups.filter(name='funcionario').exists():
             return False
         return super().has_delete_permission(request, obj)
-
+ 
     def has_change_permission(self, request, obj=None):
         if request.user.groups.filter(name='funcionario').exists():
             return False
         return super().has_change_permission(request, obj)
-
-admin.site.register(Cliente, ClienteAdmin)
-admin.site.register(Produto, ProdutoAdmin)
-admin.site.register(Venda, VendaAdmin)
-
+ 
+class CustomAdminSite(admin.AdminSite):
+    site_header = "Library Administration"
+   
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('total-revenue-by-vendor/', total_revenue_by_vendor_view, name='total_revenue_by_vendor'),
+            path('monthly-sales-by-product/', monthly_sales_by_product_view, name='monthly_sales_by_product'),
+            path('top-clients/', top_clients_view, name='top_clients'),
+            path('estatisticas/', estatisticas_view, name='estatisticas'),
+        ]
+        return my_urls + urls
+   
+admin_site = CustomAdminSite(name='custom_admin')
+ 
+admin_site.register(Cliente, ClienteAdmin)
+admin_site.register(Produto, ProdutoAdmin)
+admin_site.register(Venda, VendaAdmin)
