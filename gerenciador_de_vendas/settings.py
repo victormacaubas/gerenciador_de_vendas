31e93d2ff1c75e81a08e6dadf94b517d82f5a512
Victor Macaubas <victor@macaubas.com>
2024-06-10 23:00:02 -0300

    remove stactifiles

--- gerenciador_de_vendas/settings.py
+++ gerenciador_de_vendas/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- gerenciador_de_vendas/urls.py
+++ gerenciador_de_vendas/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gerenciador_de_vendas.gerenciador_de_vendas import settings
 from vendas_app.admin import admin_site
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', admin_site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
