d17c3eb234cd030eca4d58f9473904fbe0ddda29
Victor Macaubas <victor@macaubas.com>
2024-06-10 21:33:30 -0300

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
-FROM python:3.12.2
+FROM python:3.9-slim
 
-ENV PYTHONUNBUFFERED 1
 ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
 
 WORKDIR /app
 
-COPY requirements.txt /app/
-RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
+COPY requirements.txt /app/requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /app/
 RUN python manage.py collectstatic --noinput
--- gerenciador_de_vendas/settings.py
+++ gerenciador_de_vendas/settings.py
@@ -127,8 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = ["./vendas_app/static/vendas_app/"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
