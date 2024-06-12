853c1a8299340fb786efaf909be8f406741fb084
Victor Macaubas <victor@macaubas.com>
2024-06-11 21:11:02 -0300

    update trigger

--- vendas_app/sql/after_insert_venda.sql
+++ vendas_app/sql/after_insert_venda.sql
@@ -2,25 +2,28 @@ CREATE TRIGGER after_insert_venda
 AFTER INSERT ON vendas_app_venda
 FOR EACH ROW
 BEGIN
-    DECLARE total_sales DECIMAL(8, 2);
-    DECLARE bonus DECIMAL(8, 2);
-    DECLARE msg_txt VARCHAR(225);
+    DECLARE total_sales DECIMAL(10, 2);
+    DECLARE bonus DECIMAL(10, 2);
+    DECLARE msg_txt VARCHAR(255);
 
     SELECT SUM(valor) INTO total_sales
     FROM vendas_app_venda
-    WHERE vendedor_id = NEW.vendedor_id;
+    WHERE id_vendedor_id = NEW.id_vendedor_id;
 
     IF total_sales > 1000.00 THEN
         SET bonus = total_sales * 0.05;
         UPDATE vendas_app_funcionario
         SET is_special = TRUE,
             salario = salario + bonus
-        WHERE id = NEW.vendedor_id;
+        WHERE id = NEW.id_vendedor_id;
 
-        SET msg_txt = CONCAT('Bonus total necessário: R$ ', FORMAT(bonus,2));
+        SET msg_txt = CONCAT('Total bonus required: R$ ', FORMAT(bonus, 2));
 
-        INSERT INTO vendas_app_eventlog_message (message, created_at)
-        VALUES(msg_txt, NOW());
+        INSERT INTO eventlog_messages (message, created_at)
+        VALUES (msg_txt, NOW());
     END IF;
-    CALL RegistrarVenda(NEW.produto_id);
+
+    UPDATE vendas_app_produto
+    SET quantidade = quantidade - NEW.quantidade
+    WHERE id = NEW.id_produto_id;
 END;
